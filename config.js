f56bd0e73c65461672623a2054c89b3fbdaeb305
karas <moyongfeng@live.cn>
2017-02-10 22:15:29 +0800

    Update cross env script

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ var _ = require("lodash");
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || "development";
 env = env.trim();
-
+console.log("Env: " + env);
 var base = {
   app: {
     root: path.normalize(path.join(__dirname, "/..")),
@@ -16,32 +16,32 @@ var specific = {
   development: {
     app: {
       port: 3001,
-      name: "EagleEye platform v2 - Dev",
-      keys: [ "Refactoring" ],
+      name: "Graph demo - Dev",
+      keys: [ "Demo" ],
     },
     mongo: {
-      url: "mongodb://localhost/eagleEyeDatabase_dev",
+      url: "mongodb://localhost/graphDemoDatabase_dev",
     },
   },
   test: {
     app: {
       port: 3002,
-      name: "EagleEye platform v2 - Test",
-      keys: [ "Refactoring" ],
+      name: "Graph demo - Test",
+      keys: [ "Demo" ],
     },
     mongo: {
-      url: "mongodb://localhost/eagleEyeDatabase_test",
+      url: "mongodb://localhost/graphDemoDatabase_test",
     },
     isTest: true,
   },
   production: {
     app: {
       port: process.env.PORT || 3003,
-      name: "Refactoring",
-      keys: [ "Refactoring" ],
+      name: "Graph demo - Production",
+      keys: [ "Demo" ],
     },
     mongo: {
-      url: "mongodb://localhost/eagleEyeDatabase_prod",
+      url: "mongodb://localhost/graphDemoDatabase_prod",
     },
   },
 };
